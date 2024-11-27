5bc7e5bf08c6bc313d916aeacd31bbbb40d09180
kueb0 <karisa17_ag@hotmail.com>
2024-11-26 21:17:36 -0600

    Arlo recibe daño

--- Galactic Gardeners/Assets/Scripts/MonoBehaviours/Enemy.cs
+++ Galactic Gardeners/Assets/Scripts/MonoBehaviours/Enemy.cs
@@ -8,6 +8,12 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private GameObject efectoMuerte;
 
+    [SerializeField]
+    private float daño;
+
+    public float intervaloDaño = 1.0f;
+    private float tiempoTranscurrido = 0f;
+
     public void TomarDaño(float daño)
     {
         vida -= daño;
@@ -21,5 +27,40 @@ public class Enemy : MonoBehaviour
     {
         Instantiate(efectoMuerte, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Aplica daño inicial al entrar en contacto.
+            other.GetComponent<ArloHealth>().TomarDaño(daño);
+            tiempoTranscurrido = 0f; // Reinicia el temporizador.
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Incrementa el tiempo transcurrido mientras el jugador permanece colisionando.
+            tiempoTranscurrido += Time.deltaTime;
+
+            if (tiempoTranscurrido >= intervaloDaño)
+            {
+                // Aplica daño si se cumple el intervalo de tiempo.
+                other.GetComponent<ArloHealth>().TomarDaño(daño);
+                tiempoTranscurrido = 0f; // Reinicia el temporizador.
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Reinicia el temporizador al salir de la colisión.
+            tiempoTranscurrido = 0f;
+        }
     }
 }
